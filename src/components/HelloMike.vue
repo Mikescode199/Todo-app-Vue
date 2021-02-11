682c7a41733811ca57549a59d8ac8e582c89791d
mikecardona076 <mikecardona076@gmail.com>
2021-02-11 13:18:44 -0800

    First commit

---
+++ src/components/HelloMike.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Hola desde el componente Mikes {{ msg }} </h1>
+    
+    <!-- <b-container class="bv-example-row">
+        <b-row>
+            <b-col>1 of 3</b-col>
+            <b-col>2 of 3</b-col>
+            <b-col>3 of 3</b-col>
+        </b-row>
+    </b-container> -->
+    
+
+    
+</template>
+
+<script>
+export default {
+    name:'HelloMike',
+    
+}
+</script>
+<style scoped>
+    h1{
+        color: #000;
+    }
+
+</style>
